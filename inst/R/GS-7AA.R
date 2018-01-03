10a728ac1fbcac8358ca37335f3c4d19e94efa0b
Christian Panse <cp@fgcz.ethz.ch>
2018-01-03 21:24:35 +0100

    having both plots

--- inst/R/GS-7AA.R
+++ inst/R/GS-7AA.R
@@ -1,7 +1,9 @@
 #R
 
 # Christian Panse <cp@fgcz.ethz.ch>
+# Bernd Roschitzki 
 # Pascal Egloff <p.egloff@imm.uzh.ch>
+# 2015 - 2018
 
 # https://CRAN.R-project.org/package=protViz
 # https://github.com/cpanse/NestLink
@@ -33,7 +35,7 @@ compose_GPx10R <- function(aa_pool1, aa_pool2){
         sep='') 
   }
 
-hydrophobicity <- function(x){ ssrc(x) }
+hydrophobicity <- function(x){ unlist(lapply(x, ssrc)) }
 
 .in_silico_LCMS_map <- function(x, bins=10, ...){
   hyd <- hydrophobicity(x)
@@ -48,7 +50,7 @@ hydrophobicity <- function(x){ ssrc(x) }
     labs(x = "hydrophobicity value [as computed by SSRC; dimensionless quantity]", y = "parent ion mass [in Dalton]") +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
           panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-    scale_fill_gradientn(colours = rev(colorspace::sequential_hcl(8)))
+    scale_fill_gradientn(colours = diverge_hsv(16, h = 0, s = c(1, 0.4), v=c(0.8, 1.0), alpha = 0.5)[9:16])
   p
 }
 
@@ -59,11 +61,10 @@ aa_pool_x7 <- c(rep('A', 18), rep('S', 6), rep('T', 12), rep('N', 1), rep('Q', 1
               rep('E', 11), rep('V', 12), rep('L', 2), rep('F', 1), rep('Y', 4), rep('W', 1), 
               rep('G', 8), rep('P', 12))
 
-mycTerm <- c('WR','WLTVR','WQEGGR','WQSR','WLR')
 
 ## Compose a GSXXXXXXX(WR|WLTVR|WQGGER|WQSR|WLR) peptide
 set.seed(2)
-peptides.GSx7cTerm <- replicate(sample.size, compose_GSx7cTerm(pool=aa_pool_x7, cTerm=mycTerm))
+FC.GSx7cTerm <- replicate(sample.size, compose_GSx7cTerm(pool=aa_pool_x7))
 
 
 ## Some Sanity Checks
@@ -71,11 +72,11 @@ table(aa_pool_x7)
 
 stopifnot(length(aa_pool_x7) == 100)
 
-stopifnot(length(peptides.GSx7cTerm[grepl("^GS[ASTNQDEFVLYWGP]{7}(WR|WLTVR|WQEGGR|WLR|WQSR)$", peptides.GSx7cTerm)]) 
+stopifnot(length(FC.GSx7cTerm[grepl("^GS[ASTNQDEFVLYWGP]{7}(WR|WLTVR|WQEGGR|WLR|WQSR)$", FC.GSx7cTerm)]) 
           == sample.size)
 
 
-getFC <- function(){
+.getFC <- function(){
  
   FC <- read.table(system.file("extdata/FC.tryptic",
                                package = "NestLink"),
@@ -95,11 +96,11 @@ getFC <- function(){
   
   FC$peptideLength <- nchar(as.character(FC$peptide))
   
-  FC[idx,]
+  unique(FC[idx,])
 }
 
 
-getNB <- function(){
+.getNB <- function(){
  
   NB <- read.table(system.file("extdata/NB.tryptic", package = "NestLink"),
                    col.names = c("peptide", "ESP_Prediction"), header = TRUE)
@@ -109,34 +110,21 @@ getNB <- function(){
   NB <- NB[nchar(NB$peptide) >2, ]
   NB$ssrc <- sapply(NB$peptide, ssrc)
   NB$peptideLength <- nchar(as.character(NB$peptide))
-  NB
-}
-
-getDat <- function(){
-  
-  rr <- getFC()
-  if (input$plotFC == FALSE){
-    rr <- rr[FALSE, ]
-  }
-  if (input$plotuFC){
-    rr <- rbind(rr, getUniqueFC())
-  }
-  
-  if (input$plotNB){
-    rr <- rbind(rr, getNB())
-  }
-  if (input$plotuNB){
-    rr <- rbind(rr, getUniqueNB())
-  }
-  print(names(rr))
-  rr
+  unique(NB)
 }
 
 
 ## MAIN
-pdf("~/NestLink.pdf", 12,10)
+pdf("~/NestLink.pdf", 12,6)
 
-print(.in_silico_LCMS_map(peptides.GSx7cTerm, main='GSx7cTerm'))
+print(.in_silico_LCMS_map(FC.GSx7cTerm))
+
+p <- ggplot(rbind(.getFC(), .getNB()), aes(x=ESP_Prediction, fill=cond)) +
+  geom_histogram(bins = 50, alpha = 0.4, position="identity") +
+  labs(x = "ESP prediction") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+      panel.background = element_blank(), axis.line = element_line(colour = "black"))
+print(p)
 
 dev.off()
 
