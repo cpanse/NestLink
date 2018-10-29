702d3a64a069c18c40a4598da7b4639b6e9acbc0
Christian Panse <cp@fgcz.ethz.ch>
2018-10-29 12:50:23 +0100

    added documentation

--- exec/p1875_db11.bash
+++ exec/p1875_db11.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Christian Panse <cp@fgcz.ethz.ch> 2017
+# Christian Panse <cp@fgcz.ethz.ch> 2017-09-22
+
+# script to generate the following FASTA
+# http://fgcz-ms.uzh.ch/fasta/p1875_db11_20170922.fasta
+# Input: txt files containing two columns; 1st is used as FASTAID 2nd as AA sequences
+# Ouput: FASTA
 
 
 cat $1 \
-| awk '{print ">"$1"\n"$2}' \
-| fgcz_cut60.bash  
-> p1875_db11.fasta
+  | awk '{print ">"$1"\n"$2}' \
+  | fgcz_cut60.bash  
+  > p1875_db11.fasta
