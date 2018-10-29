e2da903f5f42be67f121700a58765e85648b8bb0
Christian Panse <cpanse@users.noreply.github.com>
2018-10-29 12:52:46 +0100

    Update p1875_db11.bash
    Former-commit-id: 147bda252a91440932da3d14fc496a69ff8d608e

--- exec/p1875_db11.bash
+++ exec/p1875_db11.bash
@@ -6,9 +6,9 @@
 # http://fgcz-ms.uzh.ch/fasta/p1875_db11_20170922.fasta
 # Input: txt files containing two columns; 1st is used as FASTAID 2nd as AA sequences
 # Output: FASTA
-# Usage: p1875_db11.bash textfile
+# Usage: p1875_db11.bash textfile > p1875_db11_20170922.fasta
 
 cat $1 \
   | awk '{print ">"$1"\n"$2}' \
   | fgcz_cut60.bash  
-  > p1875_db11.fasta
+  | tee p1875_db11.fasta
