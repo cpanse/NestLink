0a7a4a93120dc15691d513049270a826b8ad73f6
Christian Panse <cp@fgcz.ethz.ch>
2018-11-02 15:11:08 +0100

    make vignette pass

--- R/fasta.R
+++ R/fasta.R
@@ -9,7 +9,7 @@
 #' @param ... 
 #'
 #' @return
-#' @export 
+#' @export nanobodyFlycodeLinking.as.fasta
 #'
 #' @author Lennart Opitz, Christian Panse 2018
 #' @examples
@@ -20,7 +20,8 @@
 #' X<-do.call('rbind', lapply(FF, read.table, header=T, sep='\t'))
 #' cat(uniqNB2FC.as.fasta(X), file="/tmp/XX.fasta")
 #' }
-uniqNB2FC.as.fasta <- function(x, file = tempfile(fileext=".fasta"), ...){
-  idx<-1:nrow(x)
+nanobodyFlycodeLinking.as.fasta <- function(x, file = tempfile(fileext=".fasta"), ...){
+  idx <- 1:nrow(x)
+  message(paste("writting to file ", file))
   sprintf(">NB%04d_NL7Idx1 FC%d %s\n%s\n", idx, x$FlycodeCount[idx], x$NB[idx], gsub(",", "", x$AssociatedFlycodes[idx]))
 }
