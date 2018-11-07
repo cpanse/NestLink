e5068fa2366e5e86355aea0fbbbcf5b72d3e64bc
Christian Panse <cp@fgcz.ethz.ch>
2018-11-07 16:47:22 +0100

    ``` Summary: ERROR count: 0 WARNING count: 0 NOTE count: 6 For detailed information about these checks, see the BiocCheck vignette, available at https://bioconductor.org/packages/3.8/bioc/vignettes/BiocCheck/inst/doc/BiocCheck.html#interpreting-bioccheck-output ```
    ```
       'SupplementaryNote1.Rmd' using 'UTF-8' ... OK
     NONE
    * checking re-building of vignette outputs ... OK
    * checking PDF version of manual ... OK
    * DONE
    
    Status: 3 NOTEs
    See
      '/home/cp/__checkouts/R/NestLink.Rcheck/00check.log'
    for details.
    ```

--- R/fasta.R
+++ R/fasta.R
@@ -4,11 +4,11 @@
 
 #' write FASTA
 #'
-#' @param x 
-#' @param file 
-#' @param ... 
+#' @param x a \code{nanobodyFlycodeLinking} class computed by \code{\link{runNGSAnalysis}}.
+
+#' @param ... just passed
 #'
-#' @return
+#' @return sprintf stream
 #' @export nanobodyFlycodeLinking.as.fasta
 #'
 #' @author Lennart Opitz, Christian Panse 2018
@@ -17,16 +17,22 @@
 #' FF <- c('~/__projects/2018/20181030-p1875/NL7-1.extendedFrags_uniqNB2FC.txt', 
 #' '~/__projects/2018/20181030-p1875/NL7-2.extendedFrags_uniqNB2FC.txt')
 #' 
-#' X<-do.call('rbind', lapply(FF, read.table, header=T, sep='\t'))
-#' cat(uniqNB2FC.as.fasta(X), file="/tmp/XX.fasta")
+#' X <- do.call('rbind', lapply(FF, read.table, header=T, sep='\t'))
+#' cat(uniqNB2FC.as.fasta(X), file=tempfile(fileext=".fasta"))
 #' }
-nanobodyFlycodeLinking.as.fasta <- function(x, file = tempfile(fileext=".fasta"), ...){
+nanobodyFlycodeLinking.as.fasta <- function(x, ...){
   idx <- 1:nrow(x)
-  message(paste("writting to file ", file))
-  sprintf(">NB%04d_NL7Idx1 FC%d %s\n%s\n", idx, x$FlycodeCount[idx], x$NB[idx], gsub(",", "", x$AssociatedFlycodes[idx]))
+  sprintf(">NB%04d_NL7Idx1 FC%d %s\n%s\n", idx, x$FlycodeCount[idx],
+          x$NB[idx], gsub(",", "", x$AssociatedFlycodes[idx]))
 }
 
 
+#' Object Summaries of S3 class \code{nanobodyFlycodeLinking}
+#'
+#' @param object a \code{nanobodyFlycodeLinking} class computed by \code{\link{runNGSAnalysis}}.
+#'
+#' @return a data.frame object
+#' @export nanobodyFlycodeLinking.summary
 nanobodyFlycodeLinking.summary <- function(object){
   
   # number of NB
@@ -34,5 +40,6 @@ nanobodyFlycodeLinking.summary <- function(object){
   # number of AAs
   # cat(length(object))
   # cat()
+  cat ("to be implemented.")
 }
 
