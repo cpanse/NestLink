d7789f8459f643bd502152483f1a9f5441db6c7f
Christian Panse <cp@fgcz.ethz.ch>
2018-11-09 11:06:57 +0100

    added ngs test case
    Former-commit-id: 79172bbcc49f00466a74e72dbdbdb5b323c8e99c

---
+++ tests/testthat/test_ngs.R
@@ -0,0 +1,62 @@
+#R
+
+context("Test NGS")
+
+test_that("NGS ground truth", {
+  
+  f <- system.file("extdata/nanobodyFlycodeLinkage.RData", package="NestLink")
+  expect_true(file.exists(f))
+  load(f)
+  expect_true(NestLink:::is.nanobodyFlycodeLinking(nanobodyFlycodeLinkage.sample))
+  
+})
+
+
+test_that("NGS pipeline", {
+    
+    f <- system.file("extdata/nanobodyFlycodeLinkage.RData", package="NestLink")
+    expect_true(file.exists(f))
+    load(f)
+    expect_true(NestLink:::is.nanobodyFlycodeLinking(nanobodyFlycodeLinkage.sample))
+    
+    expFile <-system.file("extdata/NL42_100K.fastq.gz", package="NestLink")
+    expect_true(file.exists(expFile))
+    scratchFolder <- tempdir()
+    setwd(scratchFolder)
+    
+    
+    knownNB_File <- system.file("extdata/knownNB.txt", package="NestLink")
+    
+    
+    knownNB_data <- read.table(knownNB_File,
+                               sep='\t',
+                               header = TRUE,
+                               row.names = 1,
+                               stringsAsFactors = FALSE)
+    
+    knownNB <- Biostrings::translate(DNAStringSet(knownNB_data$Sequence))
+    names(knownNB) <- rownames(knownNB_data)
+    knownNB <- sapply(knownNB, toString)
+    
+    param <- list()
+    param[['NB_Linker1']] <- "GGCCggcggGGCC"
+    param[['NB_Linker2']] <- "GCAGGAGGA"
+    param[['ProteaseSite']] <- "TTAGTCCCAAGA"
+    param[['FC_Linker']] <- "GGCCaaggaggcCGG"
+    param[['knownNB']] <- knownNB
+    param[['nReads']] <- 100
+    param[['minRelBestHitFreq']] <- 0.8 
+    param[['minConsensusScore']] <- 0.9
+    param[['maxMismatch']] <- 1
+    param[['minNanobodyLength']] <- 348
+    param[['minFlycodeLength']] <- 33
+    param[['FCminFreq']] <- 1
+    
+    NB2FC <- runNGSAnalysis(file = expFile[1], param)
+    
+    
+    expect_true(sum((nanobodyFlycodeLinking.summary(NB2FC) == 
+                         nanobodyFlycodeLinking.summary(nanobodyFlycodeLinkage.sample))) == 3)
+})
+
+
