50955cb9dfcdb1c44c19b0d7018d05c7050eccc7
Christian Panse <cp@fgcz.ethz.ch>
2018-11-28 14:29:10 +0100

    initial

---
+++ inst/scripts/make-metadata.R
@@ -0,0 +1,56 @@
+#R
+
+### =========================================================================
+### NestLink metadata 
+### -------------------------------------------------------------------------
+###
+
+meta <- data.frame(
+    Title = c(paste0("Sample data NGS"),
+              paste0("Flycode tryptic digested"),
+              paste0("Nanobody tryptic digested"),
+              paste0("FASTA as ground-truth for unit testing"),
+              paste0("Known nanobodies"),
+              paste0("Quantitaive results for SMEG and COLI")
+              ),
+    Description = c(paste0("sample data NGS in FASTQ"),
+                    paste0("Flycode tryptic digested AA sequences with ESP_Prediction"),
+                    paste0("Nanobody tryptic digested AA sequences with ESP_Prediction"),
+                    paste0("FASTA data as ground-truth for unit testing"),
+                    paste0("known nanobodies in NC sequences"),
+                    paste0("proteomics quantitative results of nanobodies expressed in SMEG and COLI species.")
+            ),
+    BiocVersion = c("3.8"),
+    Genome = NA, 
+    SourceType = c('fastq.gz', 'txt', 'txt', 'RData', 'txt', 'csv'),
+    SourceUrl = c("https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1644",
+                  "https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1875",
+                  "https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1875",
+                  "https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1644",
+                  "https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1644",
+                  "https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1875"),
+    SourceVersion = "Nov 28 2018",
+    Species = NA,
+    TaxonomyId = NA,
+    Coordinate_1_based = NA,
+    DataProvider = "FGCZ",
+    Maintainer = c("Lennart Opitz  <lopitz@fgcz.ethz.ch>",
+                   "Christian Panse <cp@fgcz.ethz.ch>",
+                   "Christian Panse <cp@fgcz.ethz.ch>",
+                   "Lennart Opitz  <lopitz@fgcz.ethz.ch>",
+                   "Lennart Opitz  <lopitz@fgcz.ethz.ch>",
+                   "Christian Panse <cp@fgcz.ethz.ch>"),
+    RDataClass = NA ,
+    DispatchClass = NA,
+    RDataPath = c("NestLink/NL42_100K.fastq.gz",
+                  "NestLink/FC.tryptic",
+                  "NestLink/NB.tryptic",
+                  "NestLink/nanobodyFlycodeLinkage.RData",
+                  "NestLink/knownNB.txt",
+                  "NestLink/PGexport2_normalizedAgainstSBstandards_Peptides.csv"
+                  ),
+    Tags = "",
+    Notes = NA
+)
+
+write.csv(meta, file="inst/extdata/metadata.csv", row.names=FALSE)
