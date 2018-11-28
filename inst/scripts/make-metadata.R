8d08c7a0796394ae4fec7da77e68be661157d2aa
Christian Panse <cp@fgcz.ethz.ch>
2018-11-28 16:01:11 +0100

    initial
    Former-commit-id: 7b77fda83bdcfa491210bda70148eb83d877ec5a

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -5,6 +5,11 @@
 ### -------------------------------------------------------------------------
 ###
 
+# Christian Panse <cp@fgcz.ethz.ch>
+# Lennart Opitz <lopitz@fgcz.ethz.ch>
+# 20181128 1347 FGCZ
+
+
 meta <- data.frame(
     Title = c(paste0("Sample data NGS"),
               paste0("Flycode tryptic digested"),
@@ -20,7 +25,7 @@ meta <- data.frame(
                     paste0("known nanobodies in NC sequences"),
                     paste0("mass spectrometry quantitative results of nanobodies expressed in SMEG and COLI species.")
             ),
-    BiocVersion = c("3.8"),
+    BiocVersion = c("3.8", "3.9"),
     Genome = NA, 
     SourceType = c('fastq.gz', 'txt', 'txt', 'RData', 'txt', 'csv'),
     SourceUrl = c("https://fgcz-bfabric.uzh.ch/bfabric/userlab/show-project.html?id=1644",
@@ -34,13 +39,13 @@ meta <- data.frame(
     TaxonomyId = NA,
     Coordinate_1_based = NA,
     DataProvider = "FGCZ",
-    Maintainer = c("Lennart Opitz  <lopitz@fgcz.ethz.ch>",
+    Maintainer = c("Lennart Opitz <lopitz@fgcz.ethz.ch>",
                    "Christian Panse <cp@fgcz.ethz.ch>",
                    "Christian Panse <cp@fgcz.ethz.ch>",
-                   "Lennart Opitz  <lopitz@fgcz.ethz.ch>",
-                   "Lennart Opitz  <lopitz@fgcz.ethz.ch>",
+                   "Lennart Opitz <lopitz@fgcz.ethz.ch>",
+                   "Lennart Opitz <lopitz@fgcz.ethz.ch>",
                    "Christian Panse <cp@fgcz.ethz.ch>"),
-    RDataClass = NA ,
+    RDataClass = c("DNAStringSet", "data.frame", "data.frame", "data.frame","data.frame","data.frame"),
     DispatchClass = NA,
     RDataPath = c("NestLink/NL42_100K.fastq.gz",
                   "NestLink/FC.tryptic",
@@ -54,3 +59,4 @@ meta <- data.frame(
 )
 
 write.csv(meta, file="inst/extdata/metadata.csv", row.names=FALSE)
+    
