2ec56293f937c63ca09c40c818e8959af15a9191
Christian Panse <cp@fgcz.ethz.ch>
2018-12-14 11:44:50 +0100

    remove onLoad and version inc

--- R/zzz.R
+++ R/zzz.R
@@ -9,28 +9,6 @@
     }
 }
 
-#' NestLink zzz .onLoad
-#'
-#' @param libname xx
-#' @param pkgname xx
-#'
-#' @importFrom AnnotationHub query
-#' @importFrom ExperimentHub ExperimentHub
-#' @importFrom utils read.csv
-.onLoad <- function(libname, pkgname) {
-    fl <- system.file("extdata", "metadata.csv", package=pkgname)
-    metadata <- read.csv(fl, stringsAsFactors=FALSE)
-
-    # eh = ExperimentHub()
-    # load(query(eh, c("NestLink", "WU160118.RData"))[[1]])
-
-    # filename <- query(eh, c("NestLink",
-    #    "PGexport2_normalizedAgainstSBstandards_Peptides.csv"))[[1]]
-
-    #PGexport2_normalizedAgainstSBstandards_Peptides <- read.csv(filename, header = TRUE, sep=';')
-    #query(eh, "NestLink")
-    #createHubAccessors(pkgname, titles)
-}
 
 
 #' getExperimentHubFilename
